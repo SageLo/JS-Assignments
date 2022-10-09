99e35de4aa87e57307f049be8255c14dbdb47175
SageLo <115102672+SageLo@users.noreply.github.com>
2022-10-08 20:45:14 -0500

    Ass 1

---
+++ Assignments.js
@@ -0,0 +1,7 @@
+let countryName = "America";
+let continentName = 'North America';
+let populationCount = '500,000,000';
+
+console.log(countryName);
+console.log(continentName);
+console.log(populationCount);
